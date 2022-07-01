3b3956c65654a7ff973aedd512d431836975c97e
Mirsen Kahrovic <mirsenkahrovic@gmail.com>
2022-07-01 14:01:17 +0200

    added precision, speed, and click counter

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,38 @@
       return {
         x: '45%',
         y: '30%',
-        points: 0
+        points: 0,
+        misses: 0,
+        clicks: 0,
+        lastTime: Date.now(),
+        speed: 0,
+        precision: 0
       }
     },
     methods: {
-     moveCircle: function() {
-       this.x = Math.floor(Math.random() * 92) + '%';
-       this.y = Math.floor(Math.random() * 92) + '%';
-       this.points++;
-     },
+      moveCircle: function() {
+        this.x = Math.floor(Math.random() * 92) + '%';
+        this.y = Math.floor(Math.random() * 92) + '%';
+        this.points++;
+        let newTime: Date = Date.now();
+        this.speed = newTime - this.lastTime;
+        this.lastTime = newTime;
+        this.precision = Math.floor((this.points/(this.misses+this.points))*100);
+        this.clicks = this.points+this.misses;
+      },
+      handleClick: function() {
+        this.misses++;
+        this.clicks = this.points+this.misses;
+        this.precision = Math.floor((this.points/this.clicks)*100);
+      },
     },
   }
 </script>
 
 <template>
   <p class="stats" v-if="points == 0">Click the red ball to start</p>
-  <p class="stats" v-else>Score: {{points}}</p>
-  <div class="circle red" :style="{left: x, top: y}" @click="moveCircle()"></div>
+  <p class="stats" v-else>Clicks: {{ clicks }}<br/>Speed: {{ speed }}ms<br/>Precision: {{ precision }}%</p>
+  <div class="circle red" :style="{left: x, top: y}" @click="moveCircle()" v-click-outside="handleClick"></div>
 </template>
 
 <style>
@@ -45,7 +60,7 @@ body {
 }
 
 .stats {
-  color: #444;
+  color: #888;
   position: absolute; top: 50%; left: 50%;
   transform: translate(-50%, -50%);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import './samples/node-api'
+
+const clickOutside = {
+  beforeMount: (el, binding) => {
+    el.clickOutsideEvent = event => {
+      // here I check that click was outside the el and his children
+      if (!(el == event.target || el.contains(event.target))) {
+        // and if it did, call method provided in attribute value
+        binding.value();
+      }
+    };
+    document.addEventListener("click", el.clickOutsideEvent);
+  },
+  unmounted: el => {
+    document.removeEventListener("click", el.clickOutsideEvent);
+  },
+};
 
 createApp(App)
+  .directive("click-outside", clickOutside)
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
