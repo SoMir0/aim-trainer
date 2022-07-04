8b4b8b7727e8d9adc20c816695b0c668bc1e7a28
Mirsen Kahrovic <mirsenkahrovic@gmail.com>
2022-07-04 18:43:56 +0200

    added color changer, returned circle, fixed scaling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script lang="ts">
   import { defineComponent } from 'vue'
+  let colors = ['blue', 'yellow', 'blueviolet', 'orange', 'brown', 'white', 'chocolate', 'purple', 'pink', 'chartreuse', 'maroon', 'aqua', 'green', 'gray', 'red'];
+  let i = 0;
 
   export default defineComponent ({
     data() {
       return {
         x: '45%',
         y: '30%',
+        bc: 'red',
         points: 0,
         misses: 0,
         clicks: 0,
@@ -34,15 +37,27 @@
         this.clicks = this.points+this.misses;
         this.precision = Math.floor((this.points/this.clicks)*100);
       },
+      changeColor: function() {
+        this.bc = colors[i];
+        i++; if(i >= colors.length) i = 0;
+      },
+    },
+    created() {
+      let self = this;
+      window.addEventListener('keydown', (e) => { if(e.keyCode == 32) self.changeColor() });
+    },
+    destroyed() {
+      let self = this;
+      window.removeEventListener('keydown', (e) => { if(e.keyCode == 32) self.changeColor() });
     },
   })
 </script>
 
 <template>
   <p class="stats" v-if="points == 0">Click the red ball to start</p>
-  <p class="stats" v-else>Clicks: {{ clicks }}<br/>Speed: {{ speed }}ms<br/>Precision: {{ precision }}%</p>
+  <p class="stats" v-else>Clicks: {{ clicks }}<br/>Speed: {{ speed }}ms<br/>Precision: {{ precision }}%<br/>[Spacebar to change color]</p>
   <p class="pb" v-show="points != 0">Best time: {{ best }}ms<br />Average: {{ Math.floor(average/points) }}ms</p>
-  <div class="circle red" :style="{left: x, top: y}" @click="moveCircle()" v-click-outside="handleClick"></div>
+  <div class="circle" :style="{background: bc, left: x, top: y}" @click="moveCircle()" v-click-outside="handleClick"></div>
 </template>
 
 <style>
@@ -61,9 +76,10 @@ body {
 }
 
 .circle {
-  border-radius: 15%;
-  width: 5vw; height: 5vh;
+  border-radius: 50%;
+  width: 5vmin; height: 5vmin;
   position: absolute;
+  transition: 300ms;
 }
 
 .stats {
@@ -77,9 +93,5 @@ body {
   position: absolute; top: 50%; right: 5%;
   text-align: right;
   transform: translateY(-50%);
-}
-
-.red {
-  background: red;
 }
 </style>
